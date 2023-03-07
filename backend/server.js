51ea8dfb719771483c463f65afeaccb64e9de8b8
shedrack <aigbeshedrack0@gmail.com>
2023-03-07 01:30:27 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-   res.send("hello")
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("*", (req, res) => {
+   res.status(401)
+      .send("Page not found")
 })
 
 app.listen(PORT, () => {
