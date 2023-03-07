36432305cadd8676a4bb18fb0ce55a192b582c08
shedrack <aigbeshedrack0@gmail.com>
2023-03-07 02:00:08 +0000

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,31 +5,33 @@ import svgImage from '../assets/images/signup.svg';
 
 function Login() {
    return (
-      <div className='w-full flex bg-slate-500 h-screen'>
-         <div className='flex-1 bg-white flex justify-center items-center'>
-            <form className='flex flex-col gap-3 items-center max-w-[400px] w-full px-3' action="">
-               <h2 className='text-xl font-bold'>Welcome Back!!!</h2>
-               <h2>Don’t have an account? <span className='text-mainBlue'>Sign Up</span></h2>
-               <Button title={"Sign in with Google"} />
-               <div className='w-full h-9 relative flex justify-center items-center'>
-                  <div className='w-full h-[2px] bg-slate-300 rounded-lg'></div>
-                  <div className='p-1 absolute bg-white rounded-full text-sm'>OR</div>
-               </div>
-               <div className='mt-2 flex flex-col w-full gap-3'>
-                  <Input
-                     placeholder={"Email"}
-                     type="text"
-                  />
-                  <Input
-                     placeholder={"Password"}
-                     type="text"
-                  />
-                  <Button title={"Sign In"} />
-               </div>
-            </form>
-         </div>
-         <div className='flex-1 bg-white hidden md:flex justify-center items-center'>
-            <img src={svgImage} alt="image" />
+      <div className='w-full flex bg-white h-screen justify-center'>
+         <div className='w-full flex max-w-[1200px]'>
+            <div className='flex-1 bg-white flex justify-center items-center'>
+               <form className='flex flex-col gap-3 items-center max-w-[400px] w-full px-3' action="">
+                  <h2 className='text-xl font-bold'>Welcome Back!!!</h2>
+                  <h2>Don’t have an account? <span className='text-mainBlue'>Sign Up</span></h2>
+                  <Button title={"Sign in with Google"} />
+                  <div className='w-full h-9 relative flex justify-center items-center'>
+                     <div className='w-full h-[2px] bg-slate-300 rounded-lg'></div>
+                     <div className='p-1 absolute bg-white rounded-full text-sm'>OR</div>
+                  </div>
+                  <div className='flex flex-col w-full gap-3'>
+                     <Input
+                        placeholder={"Email"}
+                        type="text"
+                     />
+                     <Input
+                        placeholder={"Password"}
+                        type="text"
+                     />
+                     <Button title={"Sign In"} />
+                  </div>
+               </form>
+            </div>
+            <div className='flex-1 bg-white hidden md:flex justify-center items-center'>
+               <img src={svgImage} alt="image" />
+            </div>
          </div>
       </div>
    )
